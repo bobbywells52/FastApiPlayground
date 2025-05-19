9271b68672e6ab7bfdecbf477cd829efd8b7d15a
wellsbobby52 <wellsbobby52@gmail.com>
2025-05-19 17:00:21 -0600

    API + DB practice

--- books.py
+++ books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Body,FastAPI
 
 app = FastAPI()
 
@@ -11,6 +11,59 @@ BOOKS = [
     {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
 ]
 
+
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+    return None
+
+
+@app.get("/books/{book_author}")
+#get by path
+async def read_author(book_author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold():
+            books_to_return.append(book)
+    return books_to_return
+@app.get("/books/")
+#get by query
+async def read_by_author_query(author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == author.casefold():
+            books_to_return.append(book)
+    return books_to_return
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if (book.get('author').casefold() == book_author.casefold()
+                and book.get('category') == category.casefold()):
+            books_to_return.append(book)
+
+    return books_to_return
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+
+@app.put("/books/update_book")
+async def update_book(update_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == update_book.get('title').casefold():
+            BOOKS[i]= update_book
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
--- main.py
+++ main.py
@@ -1,16 +1,106 @@
-# This is a sample Python script.
+### return shape
+data = {
+    "items": [
+        {
+            "description": "example description",
+            "category": "example category",
+            "attributes": [
+                {
+                    "name": "attribute name",
+                    "value": "attribute value"
+                }
+            ]
+        }
+    ],
+    ## unknownWords words not tied to specific item
+    "unknownWords": ["word1", "word2"]
+}
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+blank_item = {
+            "description": "",
+            "category": "",
+            "attributes": [
+                {
+                    "name": "",
+                    "value": ""
+                }
+            ]
+        }
+
+blank_item = {
+    "description": "",
+    "category": "",
+    "attributes": [
+        {
+            "name": "",
+            "value": ""
+        }
+    ]
+}
+
+class Attribute:
+    def __init__(self, name: str, value: str):
+        self.name = name
+        self.value = value
+
+class Item:
+    def __init__(self, description: str, category: str, attributes: list[dict]):
+        self.description = description
+        self.category = category
+        self.attributes = [Attribute(attr['name'], attr['value']) for attr in attributes]
+
+def split_on_spaces(s: str) -> list[str]:
+    return s.split()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+#### function takes an input of string ex: 1/4-20 X 5/8 HEX CAP SCREW GR 5 BLK OX
+
+def generateItems(description: str):
+    #### item dictionary and unknown words list to populate
+    unknown_words = []
+    attributes = {}
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    ### map values
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    thread_size = description.split('X', 1)[0]
+
+    #### set thread size
+    attributes['Thread Size'] = thread_size
+
+    ###  drop extra space on remainder
+    remainder = description.split('X', 1)[1][1:]
+
+    length = remainder.split(' ', 1)[0]
+    attributes['Length'] = length
+
+    remainder = remainder.split(' ', 1)[1]
+
+
+
+
+
+
+    print (thread_size)
+    print (length)
+    print (split_on_spaces(remainder))
+
+    #if parsed object is uknown add to unkownd words
+
+    for word in remainder:
+        if word == 'PLN':
+            #set finish to plain
+
+        if word == 'HCS'
+            #set cat to HCS
+
+
+        else:
+            unkown_words.append(word)
+
+
+
+
+generateItems('1/4-20 X 5/8 HEX CAP SCREW GR 5 BLK OX')
+
